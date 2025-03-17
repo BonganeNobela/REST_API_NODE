29fdc5663c411b78ff8a628b0f8c1b71f41960c5
Bongani Nobela <2595626@students.wits.ac.za>
2025-03-17 02:05:07 +0200

    had to recreate api get status request since pc freezed

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen ( PORT, () => {
+
+    console.log('Server listening on PORT: ', PORT);
+
+});
+
+app.get("/status", (request, response) => {
+
+    const status = {
+
+        "Status" : "Running .."
+    };
+
+    response.send(status);
+
+});
